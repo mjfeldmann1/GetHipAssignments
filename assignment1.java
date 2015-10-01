bd205ca9338c094391f90913fe625c2305f18f2f
mjfeldmann1 <mjfeldmann1@gmail.com>
2015-09-30 21:53:43 -0500

    Update assignment1.java

--- assignment1.java
+++ assignment1.java
@@ -1,5 +1,29 @@
-public class assignment1{
+import java.util.Scanner;
+
+public class atm {
+        public static void main(String[] args) {
+
+                Scanner input = new Scanner(System.in);
+                int pin = 4321; //Correct PIN number
+                int entered; //What the user inputs
+
+                double balance = 0.00;
+
+                System.out.println("Please enter your PIN");
+                entered = input.nextInt();
+                if (entered == 4321) {
+                System.out.println("Welcome to Huge Bank Inc.");
+
+} else {
+                System.out.println("Incorrect PIN");
+}
+
+
+
+
 
 
 
 }
+}
+
