fd445c6a62d91f03c45c5c2c426c71eceee48d07
mjfeldmann1 <mjfeldmann1@gmail.com>
2015-10-01 21:28:46 -0500

    Update assignment1.java
    This adds comments to the program, and changes a few minor things

--- assignment1.java
+++ assignment1.java
@@ -1,24 +1,24 @@
 import java.util.Scanner;
 
 public class atm {
-public static int balance = 0;
+public static int balance = 0; //User's balance
         public static void main(String[] args) {
-                intro();
+                intro(); //This function shows what happens when you open the program
 
 }
 public static void intro() {
-Scanner input = new Scanner(System.in);
-int pin = 4321;
-int entered;
+Scanner input = new Scanner(System.in); //Tells what the user types in
+int pin = 4321; //User's PIN number
+int entered; //Integer that takes the place of what the user types in
 System.out.println("Please enter your PIN");
                 entered = input.nextInt();
                 if (entered == 4321) {
-                System.out.println("Welcome to Huge Bank Inc.");
-                System.out.printf("Your balance: $%s", balance);
+                System.out.println("Welcome to Huge Bank Inc."); //Welcomes user
+                System.out.printf("Your balance: $%s", balance); //Tells user balance
                 select();
 } else {
 System.out.println("Incorrect PIN");
-intro();
+intro(); //repeats intro so user can try to enter correct PIN
 }
 
 }
@@ -36,17 +36,17 @@ System.out.println("3. Exit");
 entered = input.nextInt();
         if (entered == 1) {
 System.out.println("Your choice: 1");
-deposit();
+deposit(); //This function carries out the process of making a deposit
 
 } else if (entered == 2) {
 System.out.println("Your choice: 2");
-withdraw();
+withdraw(); // This function carries out the process of making a withdrawl
 } else if (entered == 3) {
 System.out.println("Your choice: 3");
-exit();
+exit(); //This function ends the program after telling the user thank you
 } else {
 System.out.println("Please enter a number 1-3");
-select();
+select(); //If the user does not enter a number between 1 and 3, they are sent back to the selection screen
 }
 }
 
@@ -56,7 +56,8 @@ int entered;
 System.out.println("How much would you like to withdraw?");
 entered = input.nextInt();
 if (entered <= balance && entered > 0) {
-balance = balance - entered;
+balance = balance - entered; //Creates new balacne
+System.out.println("Withdrawl successful");
 System.out.printf("Your balance: $%s", balance);
 select();
 } else if (entered == 0) {
@@ -66,7 +67,7 @@ withdraw();
 
 } else {
 System.out.println("ERROR: Not enough funds");
-select();
+select(); //If the user does not have enough funds, they are sent back to the select screen
 }
 }
 
@@ -76,17 +77,12 @@ int entered;
 System.out.println("How much would you like to deposit?");
 entered = input.nextInt();
 if (entered > 0) {
-balance = entered + balance;
+balance = entered + balance; //Creates new balance
+System.out.println("Deposit successful");
 System.out.printf("Your balance: $%s", balance);
 select();
 } else if (entered == 0) {
 System.out.println("Please enter an integer other than 0");
-deposit();
-}
-}
-
-public static void exit() {
-System.out.println("Thank you for your business!");
-
+deposit(); //If the user enters 0, they are prompted to try again
 }
 }
